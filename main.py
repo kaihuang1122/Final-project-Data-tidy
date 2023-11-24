0e46a6c4639db9dd503d8238786fa6a1510745f8
Kai Huang <kaihuang1122@gmail.com>
2023-11-24 18:38:04 +0800

    init and 11/24 tidy

---
+++ main.py
@@ -0,0 +1,47 @@
+#! /Library/Frameworks/Python.framework/Versions/3.11/bin/python3
+import json
+import sys
+import os
+import numpy as np
+from datetime import datetime
+
+
+def week_convert(month, day):
+    return str(datetime(2023, month, day).weekday())
+
+
+target = np.loadtxt("/Users/kaihuang1122/Documents/ML/Final/html.2023.final.data/sno_test_set.txt")
+#/Users/kaihuang1122/Documents/ML/Final/html.2023.final.data/release/20231002/500101001.json
+#os.mkdir("1124tidy")
+prifix = "/Users/kaihuang1122/Documents/ML/Final/html.2023.final.data/release/2023"
+month = ["10", "11"]
+day10 = ["%02d"% x for x in list(range(2, 12))+list(range(15,21))+list(range(25, 32))]
+day10 = [["10", x] for x in day10]
+day11 = ["%02d"% x for x in range(1,24)]
+day11 = [["11", x] for x in day11]
+mid = "/"
+target = list(target)
+suffix = ".json"
+
+my_prifix = "/Users/kaihuang1122/Documents/ML/Final/Data tidy/1124version"
+for port in target:
+    port_fh = open(my_prifix+"/"+str(int(port))+".csv", "w")
+    for day in day10+day11:
+        print(day, int(port))
+        with open(prifix+day[0]+day[1]+mid+str(int(port))+suffix) as fh:
+            data = json.load(fh)
+
+        mdw = day[0]+','+day[1]+','+week_convert(int(day[0]), int(day[1]))+','
+        for hour in range(24):
+            for minute in range(60):
+                temp = data["%02d:%02d"%(hour, minute)]
+                if "tot" in temp:
+                    mpc = str(hour*60+minute)+','+str(temp["tot"])+','+str(temp["sbi"])
+                    port_fh.write(mdw+mpc+'\n')
+        
+
+        # print(data)
+        # print(data["00:00"])
+        # print(data["00:01"])
+    
+    port_fh.close()
