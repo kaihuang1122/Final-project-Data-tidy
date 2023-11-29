e1671c994293ce04d0989ec7d3b578c7aeadea99
Kai Huang <kaihuang1122@gmail.com>
2023-11-29 21:23:39 +0800

    fixed

--- Weather/main.py
+++ Weather/main.py
@@ -15,14 +15,15 @@ for i in table:
         new_table.append([str(epoch+(j-5)*60), str(now_epoch.month), str(now_epoch.day), str(now_epoch.hour), str(now_epoch.minute), str(now_epoch.hour*60+now_epoch.minute)]+i[3:])
 
 
-with open("/Users/kaihuang1122/Documents/ML/Final/Data tidy/NTUAS_tidy/Total/data.csv") as fin:
+with open("/Users/kaihuang1122/Documents/ML/Final/Data tidy/Weather/Weatherdata.csv", "r") as fin:
     control = list(csv.reader(fin))
 
-for i in new_table:
-    if int(i[0]) > 1701100740:
-        control.append(i)
 
-fout = open("/Users/kaihuang1122/Documents/ML/Final/Data tidy/Weather/Weatherdata.csv", "w")
-fout.write("ID (Epoch timestamp), month, day, hour, minute, accumulated minutes, temperature, rainfall, relative humidity\n")
+fout = open("/Users/kaihuang1122/Documents/ML/Final/Data tidy/Weather/Weatherdata.csv", "a")
 writer = csv.writer(fout)
-writer.writerows(control)
+for i in new_table:
+    if int(i[0]) > int(control[-1][0]):
+        writer.writerow(i)
+# fout.write("ID (Epoch timestamp), month, day, hour, minute, accumulated minutes, temperature, rainfall, relative humidity\n")
+# writer = csv.writer(fout)
+# writer.writerows(control)
