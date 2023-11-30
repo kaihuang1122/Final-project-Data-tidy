21c916ff8e5b2f667760eea90111fc66264347e1
Kai Huang <kaihuang1122@gmail.com>
2023-11-30 22:23:52 +0800

    predictUPD

---
+++ accounting/main.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import csv
+
+fh = open("/Users/kaihuang1122/Documents/ML/Final/Data tidy/Weather/Weatherdata.csv")
+table = list(csv.reader(fh))
+table = [float(x[-2]) for x in table[1:]]
+print(table)
+
+plt.hist(table)
+plt.savefig("I")
