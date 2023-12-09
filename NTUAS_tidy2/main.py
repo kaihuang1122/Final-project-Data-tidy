900675fedc49df37d36c22fb32fd57098d38aeda
Kai Huang <kaihuang1122@gmail.com>
2023-12-09 23:14:21 +0800

    12/09/23:14 tidy auto update

---
+++ NTUAS_tidy2/main.py
@@ -0,0 +1,46 @@
+import csv
+MNR2 = []
+MN = []
+
+for day in range(27, 30):
+    pathname = "202311%02d.txt"%(day+1)
+    with open("/Users/kaihuang1122/Documents/ML/Final/Data tidy/NTUAS_20231128-20231204/"+pathname) as fin:
+        table = list(fin.readlines())
+
+
+
+    for string in table:
+        temp = list(string.split(",")) + [0,0,0,0,0,0,0]
+        if("MNR2," in string):
+            MNR2.append(temp[3:5])
+        elif("MN,2023" in string):
+            MN.append([temp[3], temp[6], temp[8]])
+
+    #print(MNR2)
+    #print(MN)
+for day in range(4):
+    pathname = "202312%02d.txt"%(day+1)
+    with open("/Users/kaihuang1122/Documents/ML/Final/Data tidy/NTUAS_20231128-20231204/"+pathname) as fin:
+        table = list(fin.readlines())
+
+
+
+    for string in table:
+        temp = list(string.split(",")) + [0,0,0,0,0,0,0]
+        if("MNR2," in string):
+            MNR2.append(temp[3:5])
+        elif("MN,2023" in string):
+            MN.append([temp[3], temp[6], temp[8]])
+
+    #print(MNR2)
+    #print(MN)
+    
+fout = open("/Users/kaihuang1122/Documents/ML/Final/Data tidy/NTUAS_tidy2/Total/MNR2.csv", "w")
+writter = csv.writer(fout)
+for i in range(len(MNR2)):
+    writter.writerow(MNR2[i])
+fout = open("/Users/kaihuang1122/Documents/ML/Final/Data tidy/NTUAS_tidy2/Total/MN.csv", "w")
+writter = csv.writer(fout)
+for i in range(len(MN)):
+    writter.writerow(MN[i])
+
---
+++ NTUAS_tidy2/main2.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+import csv
+import tqdm
+
+
+MNIN = open("/Users/kaihuang1122/Documents/ML/Final/Data tidy/NTUAS_tidy2/Total/MN.csv")
+MN = list(csv.reader(MNIN))
+MNR2IN = open("/Users/kaihuang1122/Documents/ML/Final/Data tidy/NTUAS_tidy2/Total/MNR2.csv")
+MNR2 = list(csv.reader(MNR2IN))
+counting = 0
+table = []
+epoch = (datetime(2023, 11, 28, 0, 0, 0))
+
+fout = open("/Users/kaihuang1122/Documents/ML/Final/Data tidy/NTUAS_tidy2/Total/data.csv", "w")
+writter = csv.writer(fout)
+
+while epoch.timestamp() <= datetime(2023, 12, 5, 0, 0, 0).timestamp():
+    if epoch.strftime("%Y%m%d%H%M")[-4:] != "0000":
+        str = epoch.strftime("%Y%m%d%H%M")
+    else:
+        temp = datetime.fromtimestamp(epoch.timestamp()-86400)
+        str = temp.strftime("%Y%m%d24%M")
+    if(str[8:10] == "00"):
+        str[8:10] == "24"
+    temp = [int(epoch.timestamp()), epoch.month, epoch.day, epoch.hour, epoch.minute, epoch.hour*60+epoch.minute]
+    print(str)
+    prev = 0
+    temp2 = [-1, -1, -1]
+    for j in MN:
+        #print(j[0])
+        if(str == j[0]):
+            temp2[0] = j[1]
+            temp2[2] = j[2]
+            break
+        
+    for j in MNR2:
+
+        #print(j[0])
+        if(str == j[0]):
+            temp2[1] = float(j[1]) - prev
+            prev = float(j[1])
+            break
+    
+    if temp2[0] == -1:
+        temp2[0] = table[-3]
+        counting+=1
+    if temp2[1] == -1:
+        temp2[1] = table[-2]
+        counting+=1
+    if temp2[2] == -1:
+        temp2[2] = table[-1]
+        counting+=1
+
+    if -1 in temp2:
+        break
+
+    writter.writerow(temp+temp2)
+    table = (temp+temp2)
+    epoch = datetime.fromtimestamp(epoch.timestamp()+60)
+
+print(counting)
+
